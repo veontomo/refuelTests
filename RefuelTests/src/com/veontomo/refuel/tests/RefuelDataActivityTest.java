43a3c4b9fa400b1dbd538ff3f3f1b5f399059f28
Mario Rossi <veontomo@gmail.com>
2015-05-04 17:00:03 +0200

    test: control labels of the refuel data page

--- RefuelTests/src/com/veontomo/refuel/tests/RefuelDataActivityTest.java
+++ RefuelTests/src/com/veontomo/refuel/tests/RefuelDataActivityTest.java
@@ -24,9 +24,6 @@ public class RefuelDataActivityTest extends
 	private EditText mFuelStationInput;
 	private Button mBtnSave;
 	private Button mBtnCancel;
-	
-
-	
 
 	public RefuelDataActivityTest() {
 		super(RefuelDataActivity.class);
@@ -39,15 +36,22 @@ public class RefuelDataActivityTest extends
 		mKmText = (TextView) mRefuelDataActivity.findViewById(R.id.km);
 		mKmInput = (EditText) mRefuelDataActivity.findViewById(R.id.kmInput);
 		mPriceText = (TextView) mRefuelDataActivity.findViewById(R.id.price);
-		mPriceInput = (EditText) mRefuelDataActivity.findViewById(R.id.priceInput);
-		mQuantityText = (TextView) mRefuelDataActivity.findViewById(R.id.quantity);
-		mQuantityInput = (EditText) mRefuelDataActivity.findViewById(R.id.quantityInput);
+		mPriceInput = (EditText) mRefuelDataActivity
+				.findViewById(R.id.priceInput);
+		mQuantityText = (TextView) mRefuelDataActivity
+				.findViewById(R.id.quantity);
+		mQuantityInput = (EditText) mRefuelDataActivity
+				.findViewById(R.id.quantityInput);
 		mPaidText = (TextView) mRefuelDataActivity.findViewById(R.id.paid);
-		mPaidInput = (EditText) mRefuelDataActivity.findViewById(R.id.paidInput);
-		mFuelStationText = (TextView) mRefuelDataActivity.findViewById(R.id.fuelStation);
-		mFuelStationInput = (EditText) mRefuelDataActivity.findViewById(R.id.fuelStationInput);
+		mPaidInput = (EditText) mRefuelDataActivity
+				.findViewById(R.id.paidInput);
+		mFuelStationText = (TextView) mRefuelDataActivity
+				.findViewById(R.id.fuelStation);
+		mFuelStationInput = (EditText) mRefuelDataActivity
+				.findViewById(R.id.fuelStationInput);
 		mBtnSave = (Button) mRefuelDataActivity.findViewById(R.id.buttonSave);
-		mBtnCancel = (Button) mRefuelDataActivity.findViewById(R.id.buttonCancel);
+		mBtnCancel = (Button) mRefuelDataActivity
+				.findViewById(R.id.buttonCancel);
 	}
 
 	public void testPreconditions() {
@@ -60,13 +64,43 @@ public class RefuelDataActivityTest extends
 		assertNotNull("edit text for \"quantity\" is missing", mQuantityInput);
 		assertNotNull("text view for \"paid\" is missing", mPaidText);
 		assertNotNull("edit text for \"paid\" is missing", mPaidInput);
-		assertNotNull("text view for \"fuel station\" is missing", mFuelStationText);
-		assertNotNull("edit text for \"fuel station\" is missing", mFuelStationInput);
+		assertNotNull("text view for \"fuel station\" is missing",
+				mFuelStationText);
+		assertNotNull("edit text for \"fuel station\" is missing",
+				mFuelStationInput);
 		assertNotNull("button \"Save\" is missing", mBtnSave);
 		assertNotNull("button \"Cancel\" is missing", mBtnCancel);
+	}
 
+	public void testRefuelDataActivityKm() {
+		final String expected = mRefuelDataActivity.getString(R.string.km);
+		final String actual = mKmText.getText().toString();
+		assertEquals(expected, actual);
+	}
 
+	public void testRefuelDataActivityPrice() {
+		final String expected = mRefuelDataActivity.getString(R.string.price);
+		final String actual = mPriceText.getText().toString();
+		assertEquals(expected, actual);
+	}
 
+	public void testRefuelDataActivityQuantity() {
+		final String expected = mRefuelDataActivity
+				.getString(R.string.quantity);
+		final String actual = mQuantityText.getText().toString();
+		assertEquals(expected, actual);
+	}
+
+	public void testRefuelDataActivityPaid() {
+		final String expected = mRefuelDataActivity.getString(R.string.paid);
+		final String actual = mPaidText.getText().toString();
+		assertEquals(expected, actual);
+	}
+
+	public void testRefuelDataActivityFuelStation() {
+		final String expected = mRefuelDataActivity.getString(R.string.fuelStation);
+		final String actual = mFuelStationText.getText().toString();
+		assertEquals(expected, actual);
 	}
 
 }
